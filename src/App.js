4b4aac74e564b27381dc06e742015b9f9f9382d9
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-07 11:34:53 +0300

    navbar and router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import AddArticle from "./components/AddArticle";
 import Articles from "./components/Articles";
-
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-8">
-          <Articles/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={
+          <div>
+            <h1>Register</h1>
+            <h1>xyz</h1>
+          </div>
+        }/>
+        <Route path="/" element={
+          <div className="row">
+          <div className="col-md-8">
+            <Articles />
+          </div>
+          <div className="col-md-4">
+            <AddArticle />
+          </div>
         </div>
-        <div className="col-md-4">
-          <AddArticle/>
-        </div>
-      </div>
-      
-     
+        }/>
+
+        
+      </Routes>
+        <Navbar />
+        
+       
+      </BrowserRouter>
     </div>
   );
 }
